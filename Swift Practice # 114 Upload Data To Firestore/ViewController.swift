f8b4c1b6f50ae886515f782f91a391829d3db71a
Dogpa <dogpa0130@gmail.com>
2021-11-15 18:16:22 +0800

    Initial Commit

---
+++ Swift Practice # 114 Upload Data To Firestore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Practice # 114 Upload Data To Firestore
+//
+//  Created by Dogpa's MBAir M1 on 2021/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
